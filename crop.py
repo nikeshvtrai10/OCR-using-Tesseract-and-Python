faed3c18c4965967de2ef4183ace42edf118814b
Avanish Shrestha <avanissh@gmail.com>
2022-04-22 17:47:06 +0700

    Added regular expression to detect four digit code.

--- crop.py
+++ crop.py
@@ -26,7 +26,7 @@ cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
 
 # Read the image
-frame = cv2.imread(args["image"])
+frame = cv2.imread(args["image"], 0)
 frame_copy = frame.copy()
 
 # Initializing x1, y1, x2, y2
@@ -35,6 +35,7 @@ frame_copy = frame.copy()
 # Keep looping until the 'q' key is pressed
 while True:
     # Display the image and wait for a keypress
+    # frame = cv2.resize(frame, (1920, 1080))
     cv2.imshow("image", frame)
     key = cv2.waitKey(1) & 0xFF
 
@@ -54,7 +55,26 @@ cv2.destroyAllWindows()
 image_roi = frame[y1:y2, x1:x2]
 # data =  pytesseract.image_to_string(image_roi, config='--oem 3 --psm 6 tessedit_char_whitelist=0123456789 outputbase digits')
 data = pytesseract.image_to_data(image_roi, output_type=Output.DICT)
-print(data)
+# print(data.keys())
+print(data['text'])
+# print(data['conf'])
+n_boxes = len(data['text'])
+
+# date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+date_pattern = '^([0-9]{4})$'
+for i in range(n_boxes):
+    try:
+        data['text'][i]
+        data_to_check = data['text'][i]
+        # print(data_to_check)
+        if re.match(date_pattern, data_to_check):
+            print(data_to_check)
+
+    except:
+        pass
+        # else:
+        # print('No date found')
+
 cv2.imshow("Selected Region of Interest - Press any key to proceed", image_roi) 
 cv2.waitKey(0)
 
